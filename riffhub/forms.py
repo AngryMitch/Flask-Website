9b4d95d75f9c5408ca8e12da3664beebfd3b6c97
Mitchell Roemling <mitchellroemling@icloud.com>
2025-05-20 13:48:42 +1000

    Updated data model to use time, resolved review. Committing to stage then to roll to main

--- riffhub/forms.py
+++ riffhub/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import EmailField, StringField, PasswordField, SubmitField,StringField, TextAreaField, DateField, IntegerField, FileField
+from wtforms.fields import EmailField, StringField, PasswordField, SubmitField,StringField, TextAreaField, DateField, IntegerField, FileField, TimeField
 from wtforms.validators import InputRequired, EqualTo,DataRequired
 from flask_wtf.file import FileField, FileAllowed
+
 class loginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
@@ -17,22 +18,11 @@ class registerForm(FlaskForm):
 class commentForm(FlaskForm):
     body = StringField("Comment", validators=[InputRequired()])
 
-class editForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired()])
-    date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
-    time = StringField('Time', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired()])
-    capacity = IntegerField('Capacity')
-    image = FileField('Event Image', validators=[
-        FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')
-    ])
-
 class EventForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     description = TextAreaField('Description')
     date = DateField('Date', validators=[DataRequired()])
-    time = StringField('Time', validators=[DataRequired()])
+    time = TimeField('Time', format="%H:%M", validators=[DataRequired()])
     location = StringField('Location', validators=[DataRequired()])
     capacity = IntegerField('Capacity')
     image = FileField('Image')
